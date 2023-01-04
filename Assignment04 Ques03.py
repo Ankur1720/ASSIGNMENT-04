2eff358dd13361e19c8e8bebd1eff6275d87ad64
Ankur1720 <120731049+Ankur1720@users.noreply.github.com>
2023-01-04 18:19:30 +0530

    Add files via upload

---
+++ Assignment04 Ques03.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Write a Python program to square the elements of a list using map() function?
+
+
+# In[2]:
+
+
+x =[4,5,2,9]
+list1=list(map(lambda x:x**2,x))
+list1
+
+
+# In[ ]:
+
+
+
+
